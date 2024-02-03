c5e88d1daa3ce64f32bcf13112812d19f9e0ce42
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-02-03 13:30:13 -0500

    listen app

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,34 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
+
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    // Verificar autenticación
+    if (!valido) { return client.disconnect(); }
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+
+    // Ingresar al usuario a una sala en particular
+    //sala global, client.id  , 545454sdsdsd
+    client.join(uid);
+
+    // Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal',  (payload) => {
+        console.log(payload);
+
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', ( payload ) => {
